0d8ee3d73067e689dcb95eedd6e3b1772b627db4
Otavio Augusto da Costa Pimentel <OtavioAugusto.daCostaPimentel-ext@ab-inbev.com>
2021-04-27 17:08:09 -0300

    Initial Structure

---
+++ index.js
@@ -0,0 +1,54 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const { execute, stop, skip } = require("./src/commands");
+
+console.log("Rodando o BOTZÂO da massa!");
+
+const client = new Discord.Client();
+
+const prefix = "@";
+
+client.on("message", async (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(" ");
+  const command = args.shift().toLowerCase();
+
+  if (command === "test") {
+    message.reply(`Ta testando oque mermão`);
+  }
+
+  if (command === "play") {
+    if (args.length === 0) {
+      message.channel.send("Tu quer que eu adivinhe o link?");
+      return;
+    }
+
+    if (!message.member.voice.channel) {
+      message.channel.send("Cara, tu precisa ta num canal de voz, ta ligado?");
+      return;
+    }
+
+    execute(message, args[0]);
+    return;
+  }
+
+  if (command === "stop") {
+    stop(message);
+    return;
+  }
+
+  if (command === "skip") {
+    skip(message);
+    return;
+  }
+
+  if (command === "help") {
+    message.channel.send("Onde está seu deus agora? Ligue suporte ACME!!!");
+    return;
+  }
+});
+
+client.login(config.BOT_TOKEN);
---
+++ src/commands.js
@@ -0,0 +1,99 @@
+const ytdl = require("ytdl-core");
+const songList = new Map();
+
+async function execute(message, url) {
+  const serverQueue = songList.get(message.guild.id);
+
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      "You need to be in a voice channel to play music!"
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "I need the permissions to join and speak in your voice channel!"
+    );
+  }
+
+  const songInfo = await ytdl.getInfo(url);
+  const song = {
+    title: songInfo.videoDetails.title,
+    url: songInfo.videoDetails.video_url,
+  };
+
+  if (!serverQueue) {
+    const queueConstruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true,
+    };
+
+    songList.set(message.guild.id, queueConstruct);
+
+    queueConstruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueConstruct.connection = connection;
+      play(message.guild, queueConstruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      songList.delete(message.guild.id);
+      return message.channel.send(err);
+    }
+  } else {
+    serverQueue.songs.push(song);
+    return message.channel.send(`${song.title} has been added to the queue!`);
+  }
+}
+
+function play(guild, song) {
+  const serverQueue = songList.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    songList.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    })
+    .on("error", (error) => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+}
+
+function stop(message) {
+  const serverQueue = songList.get(message.guild.id);
+
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+
+  if (!serverQueue)
+    return message.channel.send("There is no song that I could stop!");
+
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+}
+
+function skip(message) {
+  const serverQueue = songList.get(message.guild.id);
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+  if (!serverQueue)
+    return message.channel.send("There is no song that I could skip!");
+  serverQueue.connection.dispatcher.end();
+}
+
+module.exports = { play, execute, stop, skip };
